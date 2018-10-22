5c11e932742adb4d133aafef8d4f8d8e1c60c2a8
Kurt <yoshi2jared@gmail.com>
2018-10-22 15:49:37 -0400

    Try to encourage 3rd rank rook lifts - take 17 - apply a smaller bonus for queens as well.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -374,6 +374,8 @@ namespace {
             // Bonus for aligning rook with enemy pawns on the same rank/file
             if (relative_rank(Us, s) >= RANK_5)
                 score += RookOnPawn * popcount(pos.pieces(Them, PAWN) & PseudoAttacks[ROOK][s]);
+			else if (relative_rank(Us, s) == RANK_3 && mob >= 4)
+				score += make_score(mob / 4 * 3, 0);
 
             // Bonus for rook on an open or semi-open file
             if (pe->semiopen_file(Us, file_of(s)))
@@ -394,6 +396,9 @@ namespace {
             Bitboard queenPinners;
             if (pos.slider_blockers(pos.pieces(Them, ROOK, BISHOP), s, queenPinners))
                 score -= WeakQueen;
+
+			if (relative_rank(Us, s) == RANK_3 && mob >= 5)
+				score += make_score(mob / 3, 0);
         }
     }
     if (T)
