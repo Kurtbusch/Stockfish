fb81eba5c3fdb201b66ef3e39c449a639509df8e
Kurt <yoshi2jared@gmail.com>
2018-10-24 13:23:00 -0400

    Bonus for bishops with high pawn asymmetry.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -348,6 +348,8 @@ namespace {
 
                 score -= BishopPawns * pe->pawns_on_same_color_squares(Us, s)
                                      * (1 + popcount(blocked & CenterFiles));
+				
+				score += make_score(pe->pawn_asymmetry(), 0);
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
