1dfc0e7df7c6c9dc45a45346b5dc6f2c5faaf560
Kurt <yoshi2jared@gmail.com>
2018-10-25 10:47:25 -0400

    Penalty for swapped pawns - take 1 - (3, 10).

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -167,6 +167,7 @@ namespace {
   constexpr Score PawnlessFlank      = S( 19, 84);
   constexpr Score RookOnPawn         = S( 10, 30);
   constexpr Score SliderOnQueen      = S( 42, 21);
+  constexpr Score SwappedPawns       = S(  3, 10);
   constexpr Score ThreatByKing       = S( 23, 76);
   constexpr Score ThreatByPawnPush   = S( 45, 40);
   constexpr Score ThreatByRank       = S( 16,  3);
@@ -344,10 +345,13 @@ namespace {
             {
                 // Penalty according to number of pawns on the same color square as the
                 // bishop, bigger when the center files are blocked with pawns.
-                Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
+				Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
 
                 score -= BishopPawns * pe->pawns_on_same_color_squares(Us, s)
                                      * (1 + popcount(blocked & CenterFiles));
+
+				Bitboard spawns = pos.pieces(Them, PAWN) & shift<Down>(pos.pieces(Us, PAWN));
+				score -= SwappedPawns * popcount(spawns);
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
