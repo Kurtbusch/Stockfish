c32201d4b859f7111d35370a2eb6af23529c9cd6
Kurt <yoshi2jared@gmail.com>
2018-11-05 16:11:27 -0500

    Double importance of rook mobility for rooks on the 1st rank.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -372,8 +372,10 @@ namespace {
         if (Pt == ROOK)
         {
             // Bonus for aligning rook with enemy pawns on the same rank/file
-            if (relative_rank(Us, s) >= RANK_5)
-                score += RookOnPawn * popcount(pos.pieces(Them, PAWN) & PseudoAttacks[ROOK][s]);
+			if (relative_rank(Us, s) >= RANK_5)
+				score += RookOnPawn * popcount(pos.pieces(Them, PAWN) & PseudoAttacks[ROOK][s]);
+			else if (relative_rank(Us, s) == RANK_1)
+				mobility[Us] += MobilityBonus[ROOK - 2][mob];
 
             // Bonus for rook on an open or semi-open file
             if (pe->semiopen_file(Us, file_of(s)))
