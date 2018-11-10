6ba21bc2f992a45632571a77318412969824b156
Kurt <yoshi2jared@gmail.com>
2018-11-10 17:44:47 -0500

    SafeCheck for pawns. Bench: 3925912

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -96,7 +96,7 @@ namespace {
   constexpr int RookSafeCheck   = 880;
   constexpr int BishopSafeCheck = 435;
   constexpr int KnightSafeCheck = 790;
-  constexpr int PawnSafeCheck   = 435;
+  constexpr int PawnSafeCheck   = 375;
 
 #define S(mg, eg) make_score(mg, eg)
 
