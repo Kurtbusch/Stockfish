88d40373c47b20daf7cb8a048d2174e1de237d3f
Kurt <yoshi2jared@gmail.com>
2018-11-20 10:26:28 -0500

    Trapped rook modification. Bench: 3848596

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -380,7 +380,7 @@ namespace {
             else if (mob <= 3)
             {
                 File kf = file_of(pos.square<KING>(Us));
-                if ((kf < FILE_E) == (file_of(s) < kf) || relative_rank(Us, s) == RANK_1)
+				if ((kf < FILE_E) == (file_of(s) < kf) || (relative_rank(Us, s) == RANK_1 && (file_of(s) == FILE_A || file_of(s) == FILE_H)))
                     score -= (TrappedRook - make_score(mob * 22, 0)) * (1 + !pos.can_castle(Us));
             }
         }
