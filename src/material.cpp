640ea2f38e339c9cf321114e18d08c97784b3820
Kurtbusch <yoshi2jared@gmail.com>
2018-12-23 21:01:18 -0500

    Tune Imbalance Parameters. Bench: 3299877

--- src/material.cpp
+++ src/material.cpp
@@ -34,23 +34,23 @@ namespace {
   constexpr int QuadraticOurs[][PIECE_TYPE_NB] = {
     //            OUR PIECES
     // pair pawn knight bishop rook queen
-    {1438                               }, // Bishop pair
-    {  40,   38                         }, // Pawn
-    {  32,  255, -62                    }, // Knight      OUR PIECES
-    {   0,  104,   4,    0              }, // Bishop
-    { -26,   -2,  47,   105,  -208      }, // Rook
-    {-189,   24, 117,   133,  -134, -6  }  // Queen
+    {1428                               }, // Bishop pair
+    {  41,   37                         }, // Pawn
+    {  29,  259, -73                    }, // Knight      OUR PIECES
+    {   0,  107,   3,    0              }, // Bishop
+    { -19,    7,  48,   108,  -199      }, // Rook
+    {-191,   16, 112,   136,  -134, -7  }  // Queen
   };
 
   constexpr int QuadraticTheirs[][PIECE_TYPE_NB] = {
     //           THEIR PIECES
     // pair pawn knight bishop rook queen
     {   0                               }, // Bishop pair
-    {  36,    0                         }, // Pawn
-    {   9,   63,   0                    }, // Knight      OUR PIECES
-    {  59,   65,  42,     0             }, // Bishop
-    {  46,   39,  24,   -24,    0       }, // Rook
-    {  97,  100, -42,   137,  268,    0 }  // Queen
+    {  27,    0                         }, // Pawn
+    {  14,   68,   0                    }, // Knight      OUR PIECES
+    {  53,   65,  39,     0             }, // Bishop
+    {  51,   37,  28,   -22,    0       }, // Rook
+    {  95,   95, -45,   142,  262,    0 }  // Queen
   };
 
   // Endgame evaluation and scaling functions are accessed directly and not through
