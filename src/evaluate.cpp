8cb12e3997ebabff2c53f2064780143698d1bcf1
Kurtbusch <yoshi2jared@gmail.com>
2018-12-26 19:17:16 -0500

    Penalize bishops when we don't have center control. Bench: 2984459

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -348,8 +348,11 @@ namespace {
                                      * (1 + popcount(blocked & CenterFiles));
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
-                if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
-                    score += LongDiagonalBishop;
+				if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
+					score += LongDiagonalBishop;
+
+				else if (!(mobilityArea[Us] & Center))
+					score -= LongDiagonalBishop;
             }
 
             // An important Chess960 pattern: A cornered bishop blocked by a friendly
