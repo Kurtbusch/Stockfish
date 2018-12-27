d51b9d60e7b157568c96fbbde87cd07ac9a4813f
Kurtbusch <yoshi2jared@gmail.com>
2018-12-26 19:06:01 -0500

    Penalize bishops more for our blocked pawns. Bench: 3322586

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -343,9 +343,9 @@ namespace {
                 // Penalty according to number of pawns on the same color square as the
                 // bishop, bigger when the center files are blocked with pawns.
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
-				Bitboard oblocked = pos.pieces(Them, PAWN) & shift<Down>(pos.pieces(Us, PAWN));
+				Bitboard blocked2 = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces(Them, PAWN));
 
-				score -= BishopPawns * (pe->pawns_on_same_color_squares(Us, s) + popcount(oblocked))
+				score -= BishopPawns * (pe->pawns_on_same_color_squares(Us, s) + popcount(blocked2))
                                      * (1 + popcount(blocked & CenterFiles));
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
