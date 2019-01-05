5662fe3e1f2e8e479245e61245f8c6034bb0d0f6
Kurtbusch <yoshi2jared@gmail.com>
2019-01-05 10:35:48 -0500

    Bonus if our king is very safe and we have more pawns. Bench: 3172139

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -163,7 +163,7 @@ namespace {
   constexpr Score PawnlessFlank      = S( 17, 95);
   constexpr Score RestrictedPiece    = S(  7,  7);
   constexpr Score RookOnPawn         = S( 10, 32);
-  constexpr Score SafeKingWithPawns  = S(  5, 30);
+  constexpr Score SafeKingWithPawns  = S(  5, 40);
   constexpr Score SliderOnQueen      = S( 59, 18);
   constexpr Score ThreatByKing       = S( 24, 89);
   constexpr Score ThreatByPawnPush   = S( 48, 39);
