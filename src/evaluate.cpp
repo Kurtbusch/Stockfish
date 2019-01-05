7b3cbeff6f5fe5c26a1edfa0b6eefebeb3ff9164
Kurtbusch <yoshi2jared@gmail.com>
2019-01-05 10:43:24 -0500

    Penalty for bishops that can't easily get to the opponent's half. Bench: 3500428

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -152,7 +152,7 @@ namespace {
   };
 
   // Assorted bonuses and penalties
-  constexpr Score BadBishop          = S( 20, 80);
+  constexpr Score BadBishop          = S( 20, 50);
   constexpr Score BishopPawns        = S(  3,  7);
   constexpr Score CloseEnemies       = S(  8,  0);
   constexpr Score CorneredBishop     = S( 50, 50);
